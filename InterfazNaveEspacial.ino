241d32df12f41562b0361e4ad8e2bd736f657f0a
echaccaya <eag07771@gmail.com>
2019-02-17 16:02:19 -0500

    Interfaz de Nave Espacial

---
+++ InterfazNaveEspacial.ino
@@ -0,0 +1,70 @@
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  prenderNave();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(2);
+
+  if(switchState==LOW) {
+    digitalWrite(3, HIGH); //Led Verde
+    digitalWrite(4, LOW); //Led Rojo
+    digitalWrite(5, LOW); //Led Rojo
+  } else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+
+    delay(250);
+
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+
+    delay(250);
+  }
+}
+void prenderNave() {  
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+
+  delay(500);
+
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+
+  delay(500);
+  
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+
+  delay(500);
+
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+
+  delay(500);
+
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+
+  delay(500);
+
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+
+  delay(500);
+}
